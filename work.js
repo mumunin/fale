183d2a6da4df9f983f9f4483a99822398b871629
mumunin <146727285+mumunin@users.noreply.github.com>
2024-02-08 07:01:21 +0700

    Add files via upload

---
+++ work.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'SW-002';
+const toCache = [
+    '/',
+    'https://cdn.jsdelivr.net/gh/mumunin/fale@main/manifest.json',
+    'https://cdn.jsdelivr.net/gh/mumunin/fale@main/reg.js',
+    'https://rawcdn.githack.com/mumunin/fale/1858dbb807b331a0a972598cec742daea32447a6/rktv.png',
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            return cache.addAll(toCache)
+        })
+        .then(self.skipWaiting())
+    )
+})
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        fetch(event.request)
+        .catch(() => {
+            return caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.match(event.request)
+            })
+        })
+    )
+})
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys()
+        .then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+            if (key !== CACHE_NAME) {
+                console.log('[ServiceWorker] Hapus cache lama', key)
+                return caches.delete(key)
+            }
+            }))
+        })
+        .then(() => self.clients.claim())
+    )
+})
