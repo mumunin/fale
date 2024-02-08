76382d4a65625ce0e8dc3973bdef85c6aaf8b65a
mumunin <146727285+mumunin@users.noreply.github.com>
2024-02-08 07:41:18 +0700

    Update work.js

--- work.js
+++ work.js
@@ -2,7 +2,7 @@ const CACHE_NAME = 'SW-002';
 const toCache = [
     '/',
     'https://rawcdn.githack.com/mumunin/fale/c8514df73b2d8d7afba29c36af17c9cdad75390b/web.webmanifest',
-    'https://rawcdn.githack.com/mumunin/fale/c8514df73b2d8d7afba29c36af17c9cdad75390b/reg.js',
+    'https://rawcdn.githack.com/mumunin/fale/e9877bd5c198abe9c86899d0f3a0f05b2e12f1bb/reg.js',
     'https://rawcdn.githack.com/mumunin/fale/1858dbb807b331a0a972598cec742daea32447a6/rktv.png',
 ];
 
